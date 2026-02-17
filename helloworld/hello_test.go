f3c301034a6f41539467f21ccc48405ba04d6d06
namnkahn1607 <nlnkandthebois@gmail.com>
2026-02-17 21:23:29 +0700

    Hello, World

---
+++ helloworld/hello_test.go
@@ -0,0 +1,37 @@
+package helloworld
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("to a person", func(t *testing.T) {
+		got := Hello("Harry", "")
+		want := "Hello, Harry"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Peter", "Spanish")
+		want := "Hola, Peter"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Parker", "French")
+		want := "Bonjour, Parker"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("wanted %s, got %s", want, got)
+	}
+}
